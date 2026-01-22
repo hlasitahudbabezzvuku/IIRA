26f636e19c0e452a5b30369f0e9674d136a84101
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-01-22 19:50:10 +0100

    Add the commons header file to UnFinity
    The commons header file now contains the most crucial basic building blocks
    for all other modules in UnFinity. Added:
    - Macros to shorten the basic GNU attributes syntax.
    - Macros for likely and unlikely condition outcome for advanced
      optimizations.
    - uf_container_of macro

---
+++ unfinity/include/uf_common.h
@@ -0,0 +1,41 @@
+#pragma once
+
+/*
+ * @brief A common header file shared between almost all other files.
+ *
+ * This file is crucial for building anything inside UnFinity. It provides all sorts of crucial functions,
+ * macros, and global variables that are required to maintain consistency between all modules. If you are
+ * thinking about creating a new module for UnFinity you'll have to use this file.
+ *
+ * @author Frantisek Lednicky (HlasitaHudbaBezZvuku)
+ * */
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h> // IWYU pragma: keep
+
+/*
+ * These macros should help by making the GNU attributes easier to write.
+ * */
+
+#define _nodiscard_ __attribute__((warn_unused_result))
+#define _malloc_ __attribute__((malloc))
+#define _alloc_size_(...) __attribute__((alloc_size(__VA_ARGS__)))
+#define _noreturn_ __attribute__((noreturn))
+#define _cleanup_(x) __attribute__((cleanup(x)))
+
+/*
+ * These macros are meant for optimizations in places where one resolute of a condition is much more likely
+ * (e.g., error return value checking, assertion, and null checks). Don't use them where you are uncertain of
+ * the condition resolute.
+ * */
+
+#define _likely_(x) (__builtin_expect(!!(x), 1))
+#define _unlikely_(x) (__builtin_expect(!!(x), 0))
+
+#define uf_container_of(ptr, type, member)                                                                   \
+    ({                                                                                                       \
+        const typeof(((type*)0)->member)* __mptr = (ptr);                                                    \
+        (type*)((char*)__mptr - offsetof(type, member));                                                     \
+    })
