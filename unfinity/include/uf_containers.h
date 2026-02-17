d26f56447723d9f309d94b5f3fcc5a61df11c987
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-02-17 22:21:28 +0100

    Create new header file for containers module
    For now I'll include only Vector definitions with some comments. After
    this I'll add the implementation for it followed by header and
    implementation for the Map.

---
+++ unfinity/include/uf_containers.h
@@ -0,0 +1,41 @@
+#pragma once
+
+/**
+ * @brief Simple containers.
+ *
+ * Yeah, contaaaiiineeers. Meet the Vector and Map; we'll be using them a lot. We don't really have any other
+ * generic data structures implemented, because we don't really need any other.
+ *
+ * Currently, we are using a `void*` backend and macro frontend. There are definitely other ways to do generic
+ * containers. I'm not fan of using `void*`, but nothing really beats this approach in simplicity and ease of
+ * debugging.
+ *
+ * Proper List implementation should be added once needed.
+ *
+ * @author Frantisek Lednicky (HlasitaHudbaBezZvuku)
+ **/
+
+#include "uf_common.h"
+
+#include <stddef.h>
+
+/**
+ * Those functions and structures are for Vectors. Vectors are simply put a resizable/dynamic arrays. Do not
+ * confuse them with Lists (linked-lists). You can read about Vectors here:
+ * https://en.wikipedia.org/wiki/Dynamic_array
+ **/
+
+typedef struct UfConVector UfConVector;
+
+UfConVector* uf_con_vector_new(size_t element_size) _nodiscard_;
+void uf_con_vector_free(UfConVector*);
+void uf_con_vector_freep(UfConVector** ptr);
+#define _autovector_ _cleanup_(uf_con_vector_freep)
+
+size_t uf_con_vector_length(const UfConVector*);
+size_t uf_con_vector_capacity(const UfConVector*);
+
+void* uf_con_vector_push(UfConVector*, const void* data);
+void* uf_con_vector_get(UfConVector*, size_t index) _nodiscard_;
+void uf_con_vector_reserve(UfConVector*, size_t capacity);
+void uf_con_vector_remove(UfConVector*, size_t index);
