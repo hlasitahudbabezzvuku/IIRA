a5ba40120588a1e08d669f536b3e6b2445005bc1
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-02-26 00:53:03 +0100

    Loop over every input file and lex it
    Now we are actually invoking the lexer.

--- iirac/source/main.c
+++ iirac/source/main.c
@@ -180,5 +180,32 @@ int main(int argc, char* argv[])
     if (config.debug) {
         uf_log_set_level(UF_LOG_DEBUG);
     }
+
+    size_t input_files_count = uf_con_vector_length(input_files);
+    if (!input_files_count) {
+        uf_log_err("No input files provided");
+        print_usage();
+        return EXIT_FAILURE;
+    }
+
+    for (size_t i = 0; i < input_files_count; i++) {
+        const char* filepath = *(const char**)uf_con_vector_get(input_files, i);
+        uf_log_info("Starting: %s", filepath);
+
+        uf_log_debug("Lexing...");
+
+        _autolexer_ LexerContext* context = lexer_context_new(filepath);
+        if (!context) {
+            uf_log_err("Could not open or process file '%s'", filepath);
+            continue;
+        }
+
+        if (config.show_lexer_output) {
+            lexer_print_debug(context);
+        }
+
+        uf_log_info("Finished: %s", filepath);
+    }
+
     return EXIT_SUCCESS;
 }
