8a27019b20dedbcce2729153d8eba88330777189
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-03-07 18:49:26 +0100

    Integrate parser from iirac main
    This actually hooks up the parser form the iirac main function. Now we
    can use it for testing.

--- iirac/source/iic_main.c
+++ iirac/source/iic_main.c
@@ -8,6 +8,7 @@
 
 #include "ii_diagnostics.h"
 #include "ii_lexer.h"
+#include "ii_parser.h"
 #include "ii_source.h"
 #include "iic_arguments.h"
 #include "uf_containers.h"
@@ -75,6 +76,20 @@ int main(const int argc, const char* argv[])
 
         if (config.stop_after_lexer) {
             uf_log_info("Stopping after lexical analysis");
+            continue;
+        }
+
+        uf_log_debug("Creating ParserContext for file '%s'", file_path);
+        _autoparser_ ParserContext* parser_context =
+            ii_parser_context_new(source, ii_lexer_get_tokens(lexer_context),
+                                  ii_lexer_get_token_count(lexer_context), diagnostic_context);
+
+        if (config.show_parser_output) {
+            ii_parser_print_debug(parser_context);
+        }
+
+        if (config.stop_after_parser) {
+            uf_log_info("Stopping after parsing into AST");
             continue;
         }
 
