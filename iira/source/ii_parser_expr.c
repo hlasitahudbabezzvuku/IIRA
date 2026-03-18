b2f583d9227a910b8f4faaf1f50adf7cabfe5e13
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-03-18 23:20:40 +0100

    Add expression parser submodule stub implementation

---
+++ iira/source/ii_parser_expr.c
@@ -0,0 +1,14 @@
+/**
+ * @brief Expression parser using Pratt top-down operator precedence.
+ * @author Frantisek Lednicky (HlasitaHudbaBezZvuku)
+ **/
+
+#include "ii_parser_expr.h"
+
+#include "ii_parser.h"
+
+AstExpr* ii_parser_parse_expression(ParserContext* context)
+{
+    (void)context;
+    return NULL;
+}
