cc4ff57b240a0e209678da7eb3d01c118eca4c27
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-03-18 16:26:56 +0100

    Add header for tracing module

---
+++ iira/include/ii_trace.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "uf_common.h"
+
+#include <stdint.h>
+#include <stdio.h>
+
+typedef struct TraceContext TraceContext;
+
+typedef struct TraceScope TraceScope;
+struct TraceScope {
+    TraceContext* context;
+    const char* func;
+    const char* file;
+    int line;
+    uint32_t depth;
+};
+
+TraceContext* ii_trace_context_new(FILE* output) _nodiscard_;
+void ii_trace_context_free(TraceContext*);
+void ii_trace_context_freep(TraceContext**);
+#define _autotrace_ _cleanup_(ii_trace_context_freep)
+
+void ii_trace_set_enabled(TraceContext*, bool enabled);
+uint32_t ii_trace_get_depth(const TraceContext*);
+
+#define TRACE_SCOPE(context)                                                                                 \
+    _autotrace_ TraceScope _trace_scope_ __attribute__((unused, cleanup(_ii_trace_scope_exit))) =            \
+        _ii_trace_scope_enter(context, __func__, __FILE__, __LINE__)
+
+#define TRACE_SCOPE_MESSAGE(context, format, ...)                                                            \
+    _autotrace_ TraceScope _trace_scope_ __attribute__((unused, cleanup(_ii_trace_scope_exit))) =            \
+        _ii_trace_scope_enter_message(context, __func__, __FILE__, __LINE__, format, ##__VA_ARGS__)
+
+void _ii_trace_log(TraceContext*, const char* file, int line, const char* format, ...)
+    __attribute__((format(printf, 4, 5)));
+
+#define TRACE_LOG(context, format, ...) ii_trace_log(context, __FILE__, __LINE__, format, ##__VA_ARGS__)
+
+TraceScope _ii_trace_scope_enter(TraceContext*, const char* func, const char* file, int line);
+TraceScope _ii_trace_scope_enter_message(TraceContext*, const char* func, const char* file, int line,
+                                         const char* format, ...) __attribute__((format(printf, 5, 6)));
+void _ii_trace_scope_exit(TraceScope*);
