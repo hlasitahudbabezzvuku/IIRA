41ac689040c06b26437527991f54e4edc24b3596
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-03-18 23:21:23 +0100

    Add statement parser submodule stub implementation

---
+++ iira/source/ii_parser_stmt.c
@@ -0,0 +1,20 @@
+/**
+ * @brief Statement parser.
+ * @author Frantisek Lednicky (HlasitaHudbaBezZvuku)
+ **/
+
+#include "ii_parser_stmt.h"
+
+#include "ii_parser.h"
+
+AstBlock* ii_parser_parse_block(ParserContext* context)
+{
+    (void)context;
+    return NULL;
+}
+
+AstStmt* ii_parser_parse_statement(ParserContext* context)
+{
+    (void)context;
+    return NULL;
+}
