9bf0a69f98ee15cf40ba27dd01f288ed6d3a3811
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-03-18 23:21:41 +0100

    Add type parser submodule stub implementation

---
+++ iira/source/ii_parser_type.c
@@ -0,0 +1,20 @@
+/**
+ * @brief Type annotation parser.
+ * @author Frantisek Lednicky (HlasitaHudbaBezZvuku)
+ **/
+
+#include "ii_parser_type.h"
+
+#include "ii_parser.h"
+
+AstType* ii_parser_parse_type(ParserContext* context)
+{
+    (void)context;
+    return NULL;
+}
+
+AstType* ii_parser_parse_type_annotation(ParserContext* context)
+{
+    (void)context;
+    return NULL;
+}
