3a0581f59ec77c1a764524cf61834b2e66374381
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-03-18 23:20:06 +0100

    Add declarations parser submodule sub implementation

---
+++ iira/source/ii_parser_decl.c
@@ -0,0 +1,14 @@
+/**
+ * @brief Top-level declaration parser.
+ * @author Frantisek Lednicky (HlasitaHudbaBezZvuku)
+ **/
+
+#include "ii_parser_decl.h"
+
+#include "ii_parser.h"
+
+AstNode* ii_parser_parse_declaration(ParserContext* context)
+{
+    (void)context;
+    return NULL;
+}
