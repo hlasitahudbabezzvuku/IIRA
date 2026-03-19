8b9bff2bf1325a28453fd9b340c87dc23102c41a
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-03-19 19:33:41 +0100

    Add proper handling of TRUE and FALSE keyword in lexer

--- iira/source/ii_lexer.c
+++ iira/source/ii_lexer.c
@@ -424,14 +424,14 @@ LexerContext* ii_lexer_context_new(Source* source, DiagnosticContext* diag_conte
     _register_keyword(context, "default", LEXER_SYM_KEY_DEFAULT);
     _register_keyword(context, "do", LEXER_SYM_KEY_DO);
     _register_keyword(context, "else", LEXER_SYM_KEY_ELSE);
-    _register_keyword(context, "false", LEXER_SYM_NUMBER);
+    _register_keyword(context, "false", LEXER_SYM_KEY_FALSE);
     _register_keyword(context, "for", LEXER_SYM_KEY_FOR);
     _register_keyword(context, "if", LEXER_SYM_KEY_IF);
     _register_keyword(context, "null", LEXER_SYM_KEY_NULL);
     _register_keyword(context, "return", LEXER_SYM_KEY_RETURN);
     _register_keyword(context, "self", LEXER_SYM_KEY_SELF);
     _register_keyword(context, "switch", LEXER_SYM_KEY_SWITCH);
-    _register_keyword(context, "true", LEXER_SYM_NUMBER);
+    _register_keyword(context, "true", LEXER_SYM_KEY_TRUE);
     _register_keyword(context, "var", LEXER_SYM_KEY_VAR);
     _register_keyword(context, "while", LEXER_SYM_KEY_WHILE);
 
