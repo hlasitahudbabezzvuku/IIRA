18b69c1d65d6ff925d849000770d1e779745a91b
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-03-25 18:58:08 +0100

    Add codegen module public header

---
+++ iirac/include/iic_codegen.h
@@ -0,0 +1,20 @@
+#pragma once
+
+/**
+ * @brief QBE Graph Builder and IL Emitter modules.
+ * @author Frantisek Lednicky (HlasitaHudbaBezZvuku)
+ **/
+
+#include "ii_ast.h"
+#include "ii_diagnostics.h"
+#include "ii_trace.h"
+
+#include <stdio.h>
+
+typedef struct CodegenContext CodegenContext;
+
+CodegenContext* iic_gen_context_new(Ast* ast, FILE* output, DiagnosticContext* diag, TraceContext*);
+void iic_gen_context_free(DiagnosticContext*);
+void iic_gen_context_freep(DiagnosticContext*);
+
+#define _autogen_ _cleanup_(ii_gen_context_freep)
